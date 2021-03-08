18d4b04674b4ff914ead67a38cc299728a7434a1
Kyle Kleven <kylekleven8@gmail.com>
2021-03-07 22:43:29 -0600

    Added seed data files

---
+++ app.js
@@ -0,0 +1,18 @@
+const 
+//inquirer script:
+
+
+// QUESTION > What would you like do to?
+
+
+// CHOICES 
+    // > View All Employees
+    // > View All Employees By Department  
+    // > View All Employees by Manager
+    // > Add Employee
+    // > Remove Employee
+    // > Remove Employee
+    // > Update Employee Role
+    // > Update Employee Manager
+
+
