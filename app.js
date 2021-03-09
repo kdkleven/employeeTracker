be2bdbc350cda325829b1ff1b56afdc0e92aba15
Kyle Kleven <kylekleven8@gmail.com>
2021-03-08 22:29:28 -0600

    Updated app.js with inquirer script for the initial menu with choices and switch statement to handle responses

--- app.js
+++ app.js
@@ -1,18 +1,81 @@
-const 
-//inquirer script:
+const mysql2 = require('mysql2');
+const inquirer = require('inquirer');
+require('dotenv').config();
+
+const connection = mysql2.createConnection(
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    process.env.DB_NAME,
+    {
+        host: 'localhost',
+        port: 3306
+    }
+);
+
+connection.connect((err) => {
+    if (err) throw err;
+    init();
+});
+
+// Initialize the application
+const init = () => {
+    // Prompt user for input response
+    inquirer
+        .prompt({
+            name: 'menu',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: [
+                'View All Employees',
+                'View All Employees By Department',
+                'View All Employees By Manager',
+                'Add Employee',
+                'Remove Employee',
+                'Update Employee Role',
+                'Update Employee Manager'
+            ],
+        })
+        .then((res) => {
+            switch (res.menu) {
+                case ('View All Employees'):
+                    viewAllEmployees();
+                    break;
+                case ('View All Employees By Department'):
+                    viewAllEmployeesByDepartment();
+                    break;
+                case ('View All Employees By Manager'):
+                    viewAllEmployeesByManager();
+                    break;
+                case ('Add Employee'):
+                    addEmployee();
+                    break;
+                case ('Remove Employee'):
+                    removeEmployee();
+                    break;
+                case ('Update Employee Role'):
+                    updateEmployeeRole();
+                    break;
+                case ('Update Employee Manager'):
+                    updateEmployeeManager();
+                    break;
+                case 'Exit':
+                    //connection.end();
+                    break;
+                default:
+                    console.log(`Something went wrong: ${res.menu}`);
+                    break;
+            };
+        }
+    );
+}
 
 
-// QUESTION > What would you like do to?
-
-
-// CHOICES 
-    // > View All Employees
-    // > View All Employees By Department  
-    // > View All Employees by Manager
-    // > Add Employee
-    // > Remove Employee
-    // > Remove Employee
-    // > Update Employee Role
-    // > Update Employee Manager
-
-
+const viewAllEmployees = () => {
+    const query =
+        'SELECT * FROM employee';
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(query);
+        init();
+    });
+};
--- config/connection.js
+++ config/connection.js
@@ -1,28 +1,18 @@
-//Use MySQL to connect to mysql database and perform queries
-//Use inquirer to interact with user via command line (see team bulider app)
-//Use console.table to display table data in the command line (practice with it)
+// const Sequelize = require('sequelize');
 
-const mysql = require("mysql");
+// // Enable access to .env variables
+// require('dotenv').config();
 
-require('dotenv').config();
+// // Use environment variables to connect to database
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306
+//   }
+// );
 
-const connection = mysql.createConnection(
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  process.env.DB_NAME,
-  {
-    host: "localhost",
-    port: 3306,
-  }
-);
-
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
-
-  //SQL CODE GOES HERE
-
-
-});
-
-
+// module.exports = sequelize;
