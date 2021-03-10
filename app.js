51741bb437b8e818f29b8e8618cfa71c9a067fc8
SageTengu <kyle.kleven@outlook.com>
2021-03-09 22:06:17 -0600

    Created sub-functions for ViewEmployeesByDepartment

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const mysql2 = require('mysql2');
+const mysql = require('mysql');
 const inquirer = require('inquirer');
+
 require('dotenv').config();
 
-const connection = mysql2.createConnection(
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    process.env.DB_NAME,
+const connection = mysql.createConnection (
     {
+        user: 'root', 
+        password: '',
+        database: 'company_db',
         host: 'localhost',
         port: 3306
     }
@@ -69,13 +70,102 @@ const init = () => {
     );
 }
 
-
 const viewAllEmployees = () => {
     const query =
-        'SELECT * FROM employee';
+        'SELECT first_name, last_name FROM employee';
     connection.query(query, (err, res) => {
         if (err) throw err;
         console.table(query);
         init();
     });
 };
+
+const viewAllEmployeesByDepartment = () => {
+    inquirer
+        .prompt({
+            name: 'menu',
+            type: 'list',
+            message: 'Which department would you like to view?',
+            choices: [
+                'Production',
+                'Research and Development',
+                'Operations',
+                'Marketing',
+                'Finance'
+            ],
+        })
+        .then((res) => {
+            switch (res.menu) {
+                case ('Production'):
+                    viewEmployeesInProduction();
+                    break;
+                case ('Research and Development'):
+                    viewEmployeesInResearchAndDevelopment();
+                    break;
+                case ('Operations'):
+                    viewEmployeesInOperations();
+                    break;
+                case ('Marketing'):
+                    viewEmployeesInMarketing();
+                    break;
+                case ('Finance'):
+                    viewEmployeesInFinance();
+                    break;
+                case 'Exit':
+                    //connection.end();
+                    break;
+                default:
+                    console.log(`Something went wrong: ${res.menu}`);
+                    break;
+            };
+        }
+    );
+};
+
+const viewEmployeesInProduction = () => {
+    const query =
+        'SELECT * FROM employee';
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        init();
+    });  
+};
+const viewEmployeesInResearchAndDevelopment = () => {
+    const query =
+        'SELECT * FROM employee';
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        init();
+    });  
+};
+const viewEmployeesInOperations = () => {
+    const query =
+        'SELECT * FROM employee';
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        init();
+    });  
+};
+const viewEmployeesInMarketing = () => {
+    const query =
+        'SELECT * FROM employee';
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        init();
+    });  
+};
+const viewEmployeesInFinance = () => {
+    const query =
+        'SELECT * FROM employee';
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        init();
+    });  
+};
+
+
--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,5 @@
--- DROP DATABASE IF EXISTS company_db;
--- CREATE DATABASE company_db;
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
 
 USE company_db;
 
